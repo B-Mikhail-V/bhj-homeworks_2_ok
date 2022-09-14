00795cafcf21266cffb77f3f3e042802f30ab50a
Mikail Bakulin <bakulin.mv@gmail.com>
2022-09-14 17:07:14 +0300

    Task_3-3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,52 +1,19 @@
-// const menuLink = document.querySelectorAll('.menu__link')
-// console.log(menuLink.length);
-// console.log(menuLink[0].href);
-// Array.from(menuLink).forEach(element => {
-  // console.log(element.className);
-  // console.log(element.closest('.menu_sub'))
+const menuLink = document.querySelectorAll('.menu__link')
 
-// })
-
-
-// let menuItem = document.querySelectorAll('.menu__item')
-
-// menuItem.forEach(element => {
-// let child = element.children
-// console.log(child);
-//   Array.from(child).forEach(el => {
-
-//   // console.log(el.className);
-//     element.onclick = () => {
-//       if(el.className == 'menu menu_sub') {
-//         el.className = 'menu menu_sub menu_active'
-//   } 
-  
-//   }
-
-// })
-// })
-
-let menuSub = document.querySelectorAll('.menu_sub')
-console.log(menuSub);
-for(let i = 0; i < menuSub.length; i++) {
-  if(menuSub[i].closest('.menu__item')) {
-    menuSub[i].closest('.menu__item').onclick = () => {
-      menuSub[i].className.add('menu_active')
+Array.from(menuLink).forEach(element => {
+  if(element.nextElementSibling) {
+      console.log(element.nextElementSibling); 
+    element.onclick = () => {
+      closeSubMenu();
+      element.nextElementSibling.classList.toggle('menu_active')
+      return false
     }
-    
   }
+});
 
+function closeSubMenu() {
+  const allSubMenu = document.querySelectorAll('.menu_active');
+  Array.from(allSubMenu).forEach(element => {
+    element.classList.remove('menu_active')
+  })
 }
-// Array.from(menuSub).forEach(el2 => {
-//   console.log(el2.closest('.menu__item'));
-//   el2.closest('.menu__item').onclick = () => {
-//     el2.className = 'menu menu_sub menu_active'
-//   }
-//   }
-// )
-
-
-
-
-
-
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,8 +3,8 @@ const arr1 = Array.from(sliderItem)
 
 let indexCurr = 0;
 
+// счетчик для стрелок слайдера
 function counter(index) {
-    // indexCurr++;
     if(index > sliderItem.length - 1) {
         index = index % sliderItem.length;
     } else if (index < -sliderItem.length) {
@@ -16,24 +16,68 @@ function counter(index) {
     return index;
 }
 
-function deactiveSlider(arr) {
-    const allSubMenu = document.querySelectorAll('.menu_active');
-    arr.forEach(element => {
+// отключение всех активных картинок
+function deactiveSlider() {
+    const allSliders = document.querySelectorAll('.slider__item');
+    allSliders.forEach(element => {
       element.classList.remove('slider__item_active')
     })
   }
 
+// отключение всех активных точек
+function deactiveDot() {
+    const allDots = document.querySelectorAll('.slider__dot');
+    allDots.forEach(element => {
 
+      element.classList.remove('slider__dot_active')
+    })
+  }
+
+// действие для стрелки вперед
 const nextClick = document.querySelector('.slider__arrow_next')
 nextClick.onclick = () => {
     indexCurr++;
-    deactiveSlider(arr1)
-    sliderItem[counter(indexCurr)].classList.add('slider__item_active')
+    deactiveSlider();
+    deactiveDot();
+    arr1[counter(indexCurr)].classList.add('slider__item_active');
+    arr2[counter(indexCurr)].classList.add('slider__dot_active');
+
+
 }
 
+// действие для стрелки назад
 const prevClick = document.querySelector('.slider__arrow_prev')
 prevClick.onclick = () => {
     indexCurr--;
-    deactiveSlider(arr1)
-    sliderItem[counter(indexCurr)].classList.add('slider__item_active')
+    deactiveSlider();
+    deactiveDot();
+    arr1[counter(indexCurr)].classList.add('slider__item_active');
+    arr2[counter(indexCurr)].classList.add('slider__dot_active');
 }
+
+// действие для точек
+const dotItem = document.querySelectorAll('.slider__dot')
+const arr2 = Array.from(dotItem);
+for(let i = 0; i < arr2.length; i++) {
+    arr2[i].onclick = () => {
+        deactiveSlider();
+        console.log(arr2);
+        deactiveDot();
+        console.log(arr2);
+        arr2[i].classList.add('slider__dot_active');
+        arr1[i].classList.add('slider__item_active');
+        console.log(arr2);
+    }
+}
+
+
+// определение индекса для активной стартовой картинки
+const indexStart = () => {
+    for(let i = 0; i < arr1.length; i++) {
+        if(arr1[i].className == 'slider__item slider__item_active')
+        return i;
+    }
+}
+
+// активирование точки, которая соответствует активной стартовой картинки
+arr2[indexStart()].classList.add('slider__dot_active')
